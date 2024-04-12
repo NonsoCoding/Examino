faca979d1b3d9182b4d71451cdda034ae98b8c38
MAC <mac@MACs-MacBook-Air.local>
2024-04-12 11:06:00 +0100

    first commit

--- FrameWork/Firebase/APIKeys.key.js
+++
@@ -1,8 +0,0 @@
-export const firebaseAPI = {
-    apiKey: "AIzaSyBmk6JDlT6Orkpqpu8GmLXGfk8o6Mf-9HA",
-  authDomain: "examino-dcdfc.firebaseapp.com",
-  projectId: "examino-dcdfc",
-  storageBucket: "examino-dcdfc.appspot.com",
-  messagingSenderId: "359979916601",
-  appId: "1:359979916601:web:0cafd8371c885cff831ce8"
-}
